9c519e84ea2f4288ae12fd5793807398f5e94779
wheogus9611 <wheogus9611@naver.com>
2020-08-13 01:46:24 +0900

    video

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://youtube:dbYoutube@youtube.cgbgr.mongodb.net/<Youtube>?retryWrites=true&w=majority' //mongoDb 연걸 부분 //<dbYoutube> - db - pwd, <youtube> db - name
+}

c7c54095fd4b632d1c73cbeab8dbee771339fe03
wheogus9611 <wheogus9611@naver.com>
2020-12-07 12:11:21 +0900

    몽고db

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://youtube:dbYoutube@test.cgbgr.mongodb.net/<Test>?retryWrites=true&w=majority' //mongoDb 연걸 부분 //<dbYoutube> - db - pwd, <youtube> db - name
-}
-
+  // mongoURL config
+};
